a6950fa79dea8cd5c38f30f293afbea9256e6d49
ATUHAIRE JUDE INNOCENT <judeinno@gmail.com.com>
2019-09-05 11:25:20 +0300

    Show how to use sync to show the row sql that creates a table

--- pseq/server.js
+++ pseq/server.js
@@ -11,11 +11,21 @@ const connection =  new Sequelize('db', 'user', 'pass', {
   operatorsAliases: false
 })
 
-connection.authenticate().then(() => {
+const User = connection.define('User', {
+  name: Sequelize.STRING,
+  bio: Sequelize.TEXT
+});
+
+connection
+  .sync({
+    logging: console.log
+  })
+  .then(() => {
   console.log('Connected to DB');
 }).catch((err) => {
   console.log(`Failled to connect to db: error = ${err}`);
 })
+
 
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
