004e1b07fd6633f70ba0fdc805b75d911fe0f1d5
ATUHAIRE JUDE INNOCENT <judeinno@gmail.com.com>
2019-09-05 10:59:36 +0300

    Add how to connect to the DB

--- pseq/server.js
+++ pseq/server.js
@@ -1,7 +1,21 @@
 const express = require('express')
+const Sequelize = require('sequelize')
 
 const app = express();
 const port = 8081;
+
+const connection =  new Sequelize('db', 'user', 'pass', {
+  host: 'localhost',
+  dialect: 'sqlite',
+  storage: 'db.sqlite',
+  operatorsAliases: false
+})
+
+connection.authenticate().then(() => {
+  console.log('Connected to DB');
+}).catch((err) => {
+  console.log(`Failled to connect to db: error = ${err}`);
+})
 
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
